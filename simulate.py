570bf668e096b25b57803d0fadedc1c4a10788eb
jannalee0819 <JannaLee2025@u.northwestern.edu>
2024-01-15 18:30:46 -0600

    initial sim

--- simulate.py
+++ simulate.py
@@ -1,1 +1,7 @@
-pass
+import dm_control.mujoco 
+import mujoco.viewer
+
+m = dm_control.mujoco.MjModel.from_xml_path(“example.xml”)
+d = dm_control.mujoco.MjData(m)
+viewer.launch_passive(m, d)
+viewer.close()
