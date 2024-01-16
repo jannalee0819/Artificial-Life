210299290920be7ead6bf5cfc3ee893bf7858348
jannalee0819 <JannaLee2025@u.northwestern.edu>
2024-01-16 13:30:49 -0600

    worm time

--- simulate.py
+++ simulate.py
@@ -1,7 +1,26 @@
-import dm_control.mujoco 
+import dm_control.mujoco
 import mujoco.viewer
+import time
+import numpy
 
-m = dm_control.mujoco.MjModel.from_xml_path(“example.xml”)
+
+m = dm_control.mujoco.MjModel.from_xml_path("example.xml")
 d = dm_control.mujoco.MjData(m)
-viewer.launch_passive(m, d)
-viewer.close()
+
+with mujoco.viewer.launch_passive(m, d) as viewer:
+    actuators = m.nu
+    wiggle = numpy.array([6, 2, 2, -1])
+    d.ctrl[:actuators] = wiggle
+
+    for i in range(10000):
+        if viewer.is_running():
+            if i%40 == 0:
+                wiggle *= -1
+            d.ctrl[:actuators] = wiggle 
+            mujoco.mj_step(m, d)
+            viewer.sync()
+            time.sleep(0.01)
+        else:
+            break
+
+    viewer.close()
